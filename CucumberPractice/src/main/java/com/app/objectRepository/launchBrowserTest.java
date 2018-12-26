163ec8804f9154e90fda72b1208fa95ada6d9852
Sharma <Sharma@DESKTOP-VAL9S0L>
2018-12-26 12:19:59 +0530

    Running TestNG inside POM.xml and build trigger from Jenins

---
+++ CucumberPractice/src/main/java/com/app/objectRepository/launchBrowserTest.java
@@ -0,0 +1,15 @@
+package com.app.objectRepository;
+
+import org.testng.annotations.Test;
+
+
+public class launchBrowserTest {
+
+	@Test
+	 void test()
+	 
+	{
+		System.out.println("im in first test case from demoOne Class");
+	}
+	
+}
